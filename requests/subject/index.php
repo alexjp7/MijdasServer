724cd2ca85aee0fc2a4bea68ffad4b4a586e57e1
Dylan Du-Bois <dylan.dubois@gmail.com>
2019-10-22 00:33:48 +1100

    testing checking of access token

--- requests/subject/index.php
+++ requests/subject/index.php
@@ -20,7 +20,7 @@
                 : badFormatRequest("No Data Posted");
     
     if(isset($data->token)) {
-        $auth = post("https://accounts.mijdas.com/api/check_token/", [""], $data->token);
+        $auth = post("https://accounts.mijdas.com/api/check_token/", ["scopes" => "tutor"], $data->token);
         badFormatRequest($auth);
         // $auth = new HttpRequest();
         // $auth->setUrl("https://accounts.mijdas.com/api/check_token/");
