20e6f9608221ebc349a68ffa63b66d64ef0b65ba
Dylan Du-Bois <dylan.dubois@gmail.com>
2019-10-22 18:26:59 +1100

    testing checking of access token

--- requests/subject/index.php
+++ requests/subject/index.php
@@ -19,59 +19,91 @@
                 ? $data->request
                 : badFormatRequest("No Data Posted");
     
-    // if(isset($data->token)) {
-    //     require "../../vendor/autoload.php";
-    //     $guzzle = new GuzzleHttp\Client(['headers' => ['Authorization' => 'Bearer ' . $data->token]]);
-    //     $response = $guzzle->request('POST', 'https://accounts.mijdas.com/api/check_token/', []);
-    //     badFormatRequest($response->getBody());
+    if(isset($data->token)) {
+        require "../../vendor/autoload.php";
+        $guzzle = new GuzzleHttp\Client(['headers' => ['Authorization' => 'Bearer ' . $data->token]]);
+        $response = $guzzle->request('POST', 'https://accounts.mijdas.com/api/check_token/', []);
+        // badFormatRequest($response->getBody());
+        $scopes = json_decode($response->getBody())->scopes;
+
+        $scope_methods = [
+            'coordinator' => [
+                'EDIT_SUBJECT',
+                'DELETE_SUBJECT',
+                'CREATE_SUBJECT',
+                'ACTIVATE_SUBJECT',
+                'ADD_STUDENTS',
+                'ADD_TUTOR',
+                'REMOVE_TUTOR',
+                'VIEW_TUTORS',
+                'VIEW_OWNED_SUBJECTS'
+            ],
+            'tutor' => [
+                'POPULATE_SUBJECTS'
+            ],
+        ];
+
+        $scope_valid = false;
+        for($i = 0; $i < count($scopes); $i++) {
+            if(in_array($request, $scope_methods[$scopes[$i]])) {
+                $scope_valid = true;
+                break;
+            }
+        }
+
+        if(!$scope_valid) {
+            badFormatRequest("Invalid token scope");
+        }
+    // } else {
+    }
+        //Defines Accessible routes based on request value
+        switch($request)
+        {
+            //Coordinator Functions
+            case "EDIT_SUBJECT":
+                include("editSubject.php");
+                break;
+            
+            case "DELETE_SUBJECT":
+                include("deleteSubject.php");
+                break;
+            
+            case "CREATE_SUBJECT":
+                include("createSubject.php");
+                break;
+            
+            case "ACTIVATE_SUBJECT":
+                include("activateSubject.php");
+                break;
+            
+            case "ADD_STUDENTS":  
+                include("addStudentToSubject.php");
+                break;
+                
+            case "ADD_TUTOR":
+                include("addTutor.php");
+                break;
+    
+            case "REMOVE_TUTOR":
+                include("removeTutor.php");
+                break;
+    
+            case "VIEW_TUTORS":
+                include("getTutors.php");
+                break;
+                
+            case "VIEW_OWNED_SUBJECTS":
+                include("viewSubjects.php");
+                break;
+            //Tutor Functions
+            case "POPULATE_SUBJECTS": 
+                include("populateSubjects.php"); 
+                break;
+    
+            default:
+                invalidMethod(arrayToString(AVAILABLE_METHODS));
+                break;
+        }
     // }
 
-    //Defines Accessible routes based on request value
-    switch($request)
-    {
-        //Coordinator Functions
-        case "EDIT_SUBJECT":
-            include("editSubject.php");
-            break;
-        
-        case "DELETE_SUBJECT":
-            include("deleteSubject.php");
-            break;
-        
-        case "CREATE_SUBJECT":
-            include("createSubject.php");
-            break;
-        
-        case "ACTIVATE_SUBJECT":
-            include("activateSubject.php");
-            break;
-        
-        case "ADD_STUDENTS":  
-            include("addStudentToSubject.php");
-            break;
-            
-        case "ADD_TUTOR":
-            include("addTutor.php");
-            break;
-
-        case "REMOVE_TUTOR":
-            include("removeTutor.php");
-            break;
-
-        case "VIEW_TUTORS":
-            include("getTutors.php");
-            break;
-            
-        case "VIEW_OWNED_SUBJECTS":
-            include("viewSubjects.php");
-            break;
-        //Tutor Functions
-        case "POPULATE_SUBJECTS": 
-            include("populateSubjects.php"); 
-            break;
-
-        default:
-            invalidMethod(arrayToString(AVAILABLE_METHODS));
-            break;
-    }
 ?>
